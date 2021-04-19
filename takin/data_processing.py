ca2c92f0beb12d250528d09809ce77b5315384ee
Yimin Jing <yymmjing@gmail.com>
2021-04-19 17:27:31 +0800

    Update data_processing.py

--- takin/data_processing.py
+++ takin/data_processing.py
@@ -42,6 +42,29 @@ def tokenize(text: str, lang="en", with_pos=False, drop_stopwords=False) -> list
         return tokenized_text
     else:
         return [ele[0] for ele in tokenized_text]
+    
+    
+def split_dataset(corpus: list, ratio: str) -> list:
+    """
+    给定一个原始数据集，打乱后，按照比例将其划分为训练集、验证集、测试集
+    """
+    random.shuffle(corpus)
+    ratio = [int(ele) for ele in ratio.split(":")]
+    length = len(corpus)
+    
+    # ratio等于3，即将corpus划分为训练集、验证集和测试集
+    if len(ratio) == 3:
+        index0 = math.ceil(length * ratio[0] / 10)
+        index1 = math.ceil(length * ratio[1] / 10)
+        index2 = length - index0 - index1
+        return corpus[:index0], corpus[index0:index0+index1], corpus[:index2]
+    # ratio等于2，即将corpus划分为训练集、测试集
+    elif len(ratio) == 2:
+        index0 = math.ceil(length * ratio[0] / 10)
+        index1 = length - index0
+        return corpus[:index0], corpus[:index1]
+    else:
+        return "ratio设置不合理，ratio=2或3"
 
 
 
