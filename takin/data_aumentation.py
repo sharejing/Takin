39ab86e3d1fc18074125ea591f95e0b81d5ee2de
Yimin Jing <yymmjing@gmail.com>
2021-04-20 09:27:50 +0800

    Update data_aumentation.py

--- takin/data_aumentation.py
+++ takin/data_aumentation.py
@@ -3,7 +3,7 @@
 @File   :   data_aumentation.py
 @Time   :   2021/04/19 16:19:12
 @Author :   Jun Quan, Qian Cao
-@Email  :   terryqj0107@gmail.com
+@Email  :   terryqj0107@gmail.com, caoqian0905@gmail.com
 @Desc   :   一些简单的数据增强函数集
 '''
 
