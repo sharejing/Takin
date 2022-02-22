abc254fe8e5f8ce4fecd0beeaa30fa1483ccadac
sharejing <1085665357@qq.com>
2022-02-22 14:52:20 +0800

    update envs

--- setup.py
+++ setup.py
@@ -12,21 +12,20 @@ from setuptools import setup
 
 setup(
     name="takin",
-    version="1.0.1",
+    version="1.1.0",
     author="Yimin Jing",
     author_email="yymmjing@gmail.com",
-    description="A Python Toolkit for File Parsing, Text Cleaning and Data Splitting",
+    description="A Python Toolkit for File Processing, Text Cleaning and Data Splitting",
     long_description=open("README.md").read(),
     license="MIT",
     url="https://github.com/sharejing/Takin",
     packages=["takin"],
-    keywords="NLP text cleaning file parsing data splitting",
+    keywords="NLP text cleaning file processing data splitting",
     python_requires=">=3.6.0",
     install_requires=[
-        "beautifulsoup4==4.10.0",
-        "python-docx==0.8.11",
-        "python-pptx==0.6.21",
-        "pdfminer3k==1.3.4"
+        "pyyaml==6.0"
+        "pandas==1.3.4"
+        "openpyxl==3.0.9"
         ],
     classifiers=[
         "Development Status :: 3 - Alpha",
